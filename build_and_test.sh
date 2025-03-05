bbd08cb8b2c2e356fc74eec22f448063456c0c4a
Alan Li <me@alanli.org>
2025-03-05 10:50:42 -0600

    another update

--- build_and_test.sh
+++ build_and_test.sh
@@ -10,7 +10,8 @@ fi
 
 executable=build/hip-matmul
 
-export HIP_CLANG_PATH=/home/xunli/llvm/bin
+# TODO: specify your own clang build. by default rocm uses clang-19 which is outdated.
+#export HIP_CLANG_PATH=~/llvm/bin
 hipcc matmul.hip -target x86_64-unknown-linux-gnu -std=c++20 -Wall -Wextra -O3 -o "${executable}" -save-temps=obj
 
 "${executable}"
